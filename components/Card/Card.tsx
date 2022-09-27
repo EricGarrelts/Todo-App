a232a60caabcfa9c47ee4760d25487f4125a3677
Eric Garrelts <ericgarrelts@Erics-MacBook-Pro.local>
2022-09-26 22:09:41 -0400

    Added create api

--- components/Card/Card.tsx
+++ components/Card/Card.tsx
@@ -2,25 +2,25 @@ import React, { useState } from "react";
 import styles from "../../styles/Card.module.css";
 
 type Props = {
-  priority: number;
-  title: string;
-  description: string;
+    priority: number;
+    header: string;
+    description: string;
 };
 
 export default function Card(props: Props) {
-  return (
-    <div
-      className={`${styles.container} ${
-        props.priority == 0 ? styles.low : ""
-      } ${props.priority == 1 ? styles.medium : ""}
+    return (
+        <div
+            className={`${styles.container} ${
+                props.priority == 0 ? styles.low : ""
+            } ${props.priority == 1 ? styles.medium : ""}
       ${props.priority == 2 ? styles.high : ""}`}
-    >
-      <div className={styles.header}>{props.title}</div>
-      <div className={styles.info}>{props.description}</div>
-      <div className={styles.footer}>
-        <div className={styles.edit}>Edit</div>
-        <div className={styles.green}>Complete</div>
-      </div>
-    </div>
-  );
+        >
+            <div className={styles.header}>{props.header}</div>
+            <div className={styles.info}>{props.description}</div>
+            <div className={styles.footer}>
+                <div className={styles.edit}>Edit</div>
+                <div className={styles.green}>Complete</div>
+            </div>
+        </div>
+    );
 }
--- components/Cards/Cards.tsx
+++ components/Cards/Cards.tsx
@@ -1,62 +1,106 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styles from "../../styles/Cards.module.css";
 import Card from "../Card/Card";
 import NewCard from "../Card/NewCard";
 
 type Props = {};
 
+type Card = {
+    id: number;
+    header: string;
+    description: string;
+    priority: number;
+    created: Date;
+};
+
 export default function Cards({}: Props) {
-  const [sortToggle, setSortToggle] = useState(false);
-  const [currentSort, setCurrentSort] = useState("date added");
-  const [newCard, setNewCard] = useState(false);
+    const [sortToggle, setSortToggle] = useState(false);
+    const [currentSort, setCurrentSort] = useState("descending");
+    const [newCard, setNewCard] = useState(false);
+    const [data, setData] = useState<Card[]>([]);
 
-  function handleSort() {
-    setSortToggle(!sortToggle);
-  }
+    const cards = data.map((card) => (
+        <Card
+            priority={card.priority}
+            header={card.header}
+            description={card.description}
+        />
+    ));
 
-  function setSort(s: string) {
-    setCurrentSort(s);
-    setSortToggle(false);
-  }
+    useEffect(() => {
+        getCards("http://localhost:3000/api/cards", currentSort);
+    }, [currentSort, newCard]);
 
-  function createCard() {
-    setNewCard(!newCard);
-  }
+    async function getCards(url: string, sort: string) {
+        const response = await fetch(url, {
+            method: "GET",
+            headers: {
+                "content-type": "application/json;charset=UTF-8",
+                sort,
+            },
+        });
+        const { success, cards } = await response.json();
+        if (success) {
+            setData(cards);
+        }
+    }
 
-  return (
-    <div className={styles.container}>
-      <div className={styles.header}>
-        <div className={styles.newcard} onClick={createCard}>
-          New Card
-        </div>
-        <div
-          onClick={handleSort}
-          className={`${styles.noselect} ${styles.button}`}
-        >
-          Sort by {currentSort}
-        </div>
+    function handleSort() {
+        setSortToggle(!sortToggle);
+    }
 
-        {!sortToggle ? null : (
-          <div className={styles.sortcontainer}>
-            <div className={styles.bold}>Sort by: </div>
-            <div onClick={() => setSort("priority")} className={styles.pointer}>
-              Priority
+    function setSort(s: string) {
+        setCurrentSort(s);
+        setSortToggle(false);
+    }
+
+    function createCard() {
+        setNewCard(!newCard);
+    }
+
+    return (
+        <div className={styles.container}>
+            <div className={styles.header}>
+                <div className={styles.newcard} onClick={createCard}>
+                    New Card
+                </div>
+                <div
+                    onClick={handleSort}
+                    className={`${styles.noselect} ${styles.button}`}
+                >
+                    Sort by {currentSort}
+                </div>
+
+                {!sortToggle ? null : (
+                    <div className={styles.sortcontainer}>
+                        <div className={styles.bold}>Sort by: </div>
+                        <div
+                            onClick={() => setSort("priority")}
+                            className={styles.pointer}
+                        >
+                            Priority
+                        </div>
+                        <div
+                            onClick={() => setSort("descending")}
+                            className={styles.pointer}
+                        >
+                            Descending
+                        </div>
+                        <div
+                            onClick={() => setSort("ascending")}
+                            className={styles.pointer}
+                        >
+                            Ascending
+                        </div>
+                    </div>
+                )}
             </div>
-            <div
-              onClick={() => setSort("date added")}
-              className={styles.pointer}
-            >
-              Date Added
-            </div>
-          </div>
-        )}
-      </div>
 
-      {/* Cards*/}
-      <div className={styles.cardcontainer}>
-        {newCard ? <NewCard createCard={createCard} /> : ""}
-        <Card priority={0} title="Lorem ipsum" description="test 123123123" />
-      </div>
-    </div>
-  );
+            {/* Cards*/}
+            <div className={styles.cardcontainer}>
+                {newCard ? <NewCard createCard={createCard} /> : ""}
+                {cards}
+            </div>
+        </div>
+    );
 }
